d5368a1565029ab4050e86bc85df1cbcb95a476c
Justin Hao <justinhao@live.com>
2023-09-13 15:48:34 -0400

    Add get tweets from backend

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,32 +6,7 @@
 
 $(document).ready(function () {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
+  // Creates the tweet element
   const createTweetElement = function (tweet) {
     let $tweet = $(`
     <article class="tweet">
@@ -62,12 +37,31 @@ $(document).ready(function () {
     $('.tweet-container').append($tweet);
   }
 
+  //Loops through an array of tweet objects, then calls createTweetElement to generate a new <article> for each tweet
   const renderTweets = function (tweetArr) {
     for (let tweet of tweetArr) {
       createTweetElement(tweet);
     }
   }
 
-  renderTweets(data);
+  
+  // POSTs /tweets when new tweet form is submitted
+  $(function () {
+    $('.newTweetForm').on('submit', function (event) {
+      event.preventDefault();
+      console.log($(this).serialize())
+      $.post('/tweets', $(this).serialize())
+    })
+  })
+  
+  // GETs tweets and renders them
+  const loadTweets = function () {
+    $.get('/tweets', function (tweets) {
+      renderTweets(tweets);
+    })
+  }
+
+  // Calling functions to generate content on page
+  loadTweets();
 
 })
