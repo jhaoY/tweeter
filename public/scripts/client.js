4def0bc3cd5a0513cef5fc1bf363b42b2185f3c1
Justin Hao <justinhao@live.com>
2023-09-13 22:53:57 -0400

    Replace alert error messages with proper errors

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -49,16 +49,25 @@ $(document).ready(function () {
   $(function () {
     $('.newTweetForm').on('submit', function (event) {
       event.preventDefault();
-      const tweetValue = $('#tweet-text').val() 
-      if (tweetValue === '') {
-        alert('Please enter a tweet!')
-      } else if (tweetValue.length > 140) {
-        alert('Tweet is too long, please ensure it is below 140 characters')
-      } else {
-        $('.tweet').empty();
-        $.post('/tweets', $(this).serialize())
-          .then(() => loadTweets())
-      }
+      const tweetValue = $('#tweet-text').val()
+      const $errMsg = $('.error-message')
+
+      $errMsg.slideUp(function() {
+        if (tweetValue === '') {
+          const $errMsg = $('.error-message').text('Whoops, looks like your tweet is empty!')
+          $errMsg.prepend('<i class="fa-solid fa-circle-exclamation" style="color: #e10e0e;"></i>')
+          $errMsg.append('<i class="fa-solid fa-circle-exclamation" style="color: #e10e0e;"></i>')
+        } else if (tweetValue.length > 140) {
+          const $errMsg = $('.error-message').text('Tweet is too long, please ensure it is below 140 characters')
+          $errMsg.prepend('<i class="fa-solid fa-circle-exclamation" style="color: #e10e0e;"></i>')
+          $errMsg.append('<i class="fa-solid fa-circle-exclamation" style="color: #e10e0e;"></i>')
+        } else {
+          $('.tweet').empty();
+          $.post('/tweets', $(this).serialize())
+            .then(() => loadTweets())
+        }
+        $errMsg.slideDown();
+      })
     })
   })
 
